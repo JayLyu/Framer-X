efb08e31f45333dc23e23a88926a1a8bff37a737
baikong <86656944@qq.com>
2019-10-02 16:09:23 +0800

    update LoadLocalImage

--- LoadLocalImage.framerfx/code/LoadLocalImage.tsx
+++ LoadLocalImage.framerfx/code/LoadLocalImage.tsx
@@ -1,46 +1,22 @@
 import * as React from "react"
+import { useState } from "react"
 import { Frame, addPropertyControls, ControlType } from "framer"
 
 const GrayImage =
     "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAYAAABw4pVUAAAAAXNSR0IArs4c6QAAAW1JREFUeAHt01ENACAMxFDAv8C5gQQV/XhT0LS7PTN3uYyBkyEB8g0IEnsEQQSJGYjhWIggMQMxHAsRJGYghmMhgsQMxHAsRJCYgRiOhQgSMxDDsRBBYgZiOBYiSMxADMdCBIkZiOFYiCAxAzEcCxEkZiCGYyGCxAzEcCxEkJiBGI6FCBIzEMOxEEFiBmI4FiJIzEAMx0IEiRmI4ViIIDEDMRwLESRmIIZjIYLEDMRwLESQmIEYjoUIEjMQw7EQQWIGYjgWIkjMQAzHQgSJGYjhWIggMQMxHAsRJGYghmMhgsQMxHAsRJCYgRiOhQgSMxDDsRBBYgZiOBYiSMxADMdCBIkZiOFYiCAxAzEcCxEkZiCGYyGCxAzEcCxEkJiBGI6FCBIzEMOxEEFiBmI4FiJIzEAMx0IEiRmI4ViIIDEDMRwLESRmIIZjIYLEDMRwLESQmIEYjoUIEjMQw7EQQWIGYjgWIkjMQAzHQmJBHm4eBE9r/MyjAAAAAElFTkSuQmCC"
 
-export class LoadLocalImage extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            src: GrayImage,
-        }
-        this.handleChangeFile = this.handleChangeFile.bind(this)
-    }
+export function LoadLocalImage(props) {
+    const [img, setImg] = useState(GrayImage)
+    const [intro, setIntro] = useState(true)
 
-    render() {
-        return (
-            <label>
-                <input
-                    style={{ display: "none" }}
-                    type="file"
-                    accept="image/*"
-                    onChange={e => this.handleChangeFile(e)}
-                />
-                <img
-                    id="test"
-                    ref="image"
-                    height="100%"
-                    width="100%"
-                    src={this.state.src}
-                />
-            </label>
-        )
-    }
-
-    handleChangeFile = e => {
+    function handleChangeFile(e) {
         let file = e.target.files[0]
-        let preview = this.refs.image
         let reader = new FileReader()
         reader.addEventListener(
             "load",
             function() {
-                preview.src = reader.result
+                setImg(reader.result)
+                setIntro(false)
             },
             false
         )
@@ -48,4 +24,41 @@ export class LoadLocalImage extends React.Component {
             reader.readAsDataURL(file)
         }
     }
+
+    return (
+        <label>
+            <input
+                style={{ display: "none" }}
+                type="file"
+                accept="image/*"
+                onChange={e => handleChangeFile(e)}
+            />
+            <div
+                style={{
+                    display: intro ? "flex" : "none",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    position: "absolute",
+                    width: "100%",
+                    height: "100%",
+                    background: "rgba(98,54,255,0.1)",
+                    border: "1px solid rgba(98,54,255,1)",
+                    fontSize: "1rem",
+                    color: "rgba(98,54,255,1)",
+                }}
+            >
+                Drag a image to here.
+            </div>
+            <img
+                alt={"upload image"}
+                style={{
+                    position: "absolute",
+                    display: intro ? "none" : "block",
+                    width: "100%",
+                    height: "100%",
+                }}
+                src={img}
+            />
+        </label>
+    )
 }
---
+++ TMcomponent.framerfx/build/vendors.js
@@ -0,0 +1,231 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["vendors~main"],{
+
+/***/ "./designDependencies.js":
+/*!*******************************!*\
+  !*** ./designDependencies.js ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+const designs = {}
+designs["@framer/baikong.tmall-icons"] = {
+  "root" : {
+    "__class" : "RootNode",
+    "children" : [
+      {
+        "__class" : "FrameNode",
+        "aspectRatio" : 1,
+        "blendingEnabled" : 0,
+        "blendingMode" : "normal",
+        "blur" : 12,
+        "blurEnabled" : 0,
+        "blurType" : "layer",
+        "borderBottom" : 1,
+        "borderColor" : "#222",
+        "borderEnabled" : 0,
+        "borderLeft" : 1,
+        "borderPerSide" : false,
+        "borderRight" : 1,
+        "borderStyle" : "solid",
+        "borderTop" : 1,
+        "borderWidth" : 1,
+        "bottom" : null,
+        "boxShadows" : [
+
+        ],
+        "brightness" : 100,
+        "brightnessEnabled" : 0,
+        "centerAnchorX" : 0.25,
+        "centerAnchorY" : 1.375,
+        "children" : [
+          {
+            "__class" : "SVGNode",
+            "aspectRatio" : 1,
+            "blendingEnabled" : 0,
+            "blendingMode" : "normal",
+            "blur" : 12,
+            "blurEnabled" : 0,
+            "blurType" : "layer",
+            "bottom" : null,
+            "brightness" : 100,
+            "brightnessEnabled" : 0,
+            "centerAnchorX" : 0.5,
+            "centerAnchorY" : 0.5,
+            "children" : [
+
+            ],
+            "colorable" : true,
+            "constraintsLocked" : false,
+            "contrast" : 100,
+            "contrastEnabled" : 0,
+            "exportOptions" : [
+              {
+                "__class" : "ExportOptions",
+                "enabled" : true,
+                "nameExtension" : "",
+                "nameExtensionMode" : "Suffix",
+                "scale" : 1,
+                "type" : "png"
+              }
+            ],
+            "fillColor" : "#555555",
+            "fillImage" : null,
+            "fillImagePixelHeight" : null,
+            "fillImagePixelWidth" : null,
+            "fillImageResize" : "fill",
+            "fillLinearGradient" : {
+              "__class" : "LinearGradient",
+              "alpha" : 1,
+              "angle" : 180,
+              "stops" : [
+                {
+                  "__class" : "GradientColorStop",
+                  "position" : 0,
+                  "value" : "rgba(0,0,0,0)"
+                },
+                {
+                  "__class" : "GradientColorStop",
+                  "position" : 1,
+                  "value" : "black"
+                }
+              ]
+            },
+            "fillType" : "color",
+            "grayscale" : 0,
+            "grayScaleEnabled" : 0,
+            "height" : 24,
+            "heightType" : 0,
+            "hueRotate" : 0,
+            "hueRotateEnabled" : 0,
+            "id" : "@framer\/colebemis.feather-icons-AQONeGMFK-KTEQr_Dpj",
+            "intrinsicHeight" : 24,
+            "intrinsicWidth" : 24,
+            "invert" : 0,
+            "invertEnabled" : 0,
+            "left" : 0,
+            "locked" : false,
+            "name" : null,
+            "opacity" : 1,
+            "originalFilename" : "activity.svg",
+            "originalid" : null,
+            "parentid" : "@framer\/colebemis.feather-icons-AQONeGMFK",
+            "right" : 0,
+            "rotation" : 0,
+            "saturate" : 100,
+            "saturateEnabled" : 0,
+            "sepia" : 0,
+            "sepiaEnabled" : 0,
+            "shadows" : [
+
+            ],
+            "svg" : "<?xml version=\"1.0\"?><svg xmlns=\"http:\/\/www.w3.org\/2000\/svg\" width=\"24\" height=\"24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"feather feather-activity\"><path d=\"M22 12h-4l-3 9L9 3l-3 9H2\"><\/path><\/svg>",
+            "top" : 0,
+            "visible" : true,
+            "width" : 24,
+            "widthType" : 0
+          }
+        ],
+        "clip" : true,
+        "codeOverrideEnabled" : false,
+        "constraintsLocked" : false,
+        "contrast" : 100,
+        "contrastEnabled" : 0,
+        "deviceType" : null,
+        "exportOptions" : [
+          {
+            "__class" : "ExportOptions",
+            "enabled" : true,
+            "nameExtension" : "",
+            "nameExtensionMode" : "Suffix",
+            "scale" : 1,
+            "type" : "png"
+          }
+        ],
+        "fillColor" : "rgba(255,255,255,1)",
+        "fillEnabled" : false,
+        "fillImage" : null,
+        "fillImagePixelHeight" : null,
+        "fillImagePixelWidth" : null,
+        "fillImageResize" : "fill",
+        "fillLinearGradient" : {
+          "__class" : "LinearGradient",
+          "alpha" : 1,
+          "angle" : 180,
+          "stops" : [
+            {
+              "__class" : "GradientColorStop",
+              "position" : 0,
+              "value" : "rgba(0,0,0,0)"
+            },
+            {
+              "__class" : "GradientColorStop",
+              "position" : 1,
+              "value" : "black"
+            }
+          ]
+        },
+        "fillType" : "color",
+        "grayscale" : 0,
+        "grayScaleEnabled" : 0,
+        "height" : 24,
+        "heightType" : 0,
+        "hueRotate" : 0,
+        "hueRotateEnabled" : 0,
+        "id" : "@framer\/colebemis.feather-icons-AQONeGMFK",
+        "intrinsicHeight" : null,
+        "intrinsicWidth" : null,
+        "invert" : 0,
+        "invertEnabled" : 0,
+        "isExternalMaster" : "@framer\/colebemis.feather-icons",
+        "isMaster" : true,
+        "isTarget" : false,
+        "left" : 0,
+        "locked" : false,
+        "name" : "activity",
+        "navigationTarget" : null,
+        "navigationTransition" : "push",
+        "navigationTransitionBackdropColor" : "rgba(4,4,15,.4)",
+        "navigationTransitionDirection" : "left",
+        "opacity" : 1,
+        "originalid" : null,
+        "parentid" : "tUuk3SC0M",
+        "previewSettings" : null,
+        "radius" : 0,
+        "radiusBottomLeft" : 0,
+        "radiusBottomRight" : 0,
+        "radiusIsRelative" : false,
+        "radiusPerCorner" : false,
+        "radiusTopLeft" : 0,
+        "radiusTopRight" : 0,
+        "replicaInfo" : null,
+        "right" : null,
+        "rotation" : 0,
+        "saturate" : 100,
+        "saturateEnabled" : 0,
+        "sepia" : 0,
+        "sepiaEnabled" : 0,
+        "top" : 0,
+        "visible" : true,
+        "width" : 24,
+        "widthType" : 0
+      }
+    ],
+    "id" : "tUuk3SC0M",
+    "name" : null,
+    "originalid" : null,
+    "parentid" : null,
+    "tokens" : {
+
+    },
+    "tokensIndex" : [
+
+    ],
+    "visible" : false
+  },
+  "version" : 52
+}
+module.exports = designs
+
+/***/ })
+
+}]);
